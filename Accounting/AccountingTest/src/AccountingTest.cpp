d8751a2c23024f26973ba4c3d4850818af9cc774
IrvingLu <irvinglu@reallusion.com>
2018-11-30 17:39:51 +0800

    project template ready.

---
+++ Accounting/AccountingTest/src/AccountingTest.cpp
@@ -0,0 +1,17 @@
+// AccountingTest.cpp : Defines the entry point for the console application.
+//
+
+#include <gtest/gtest.h>
+
+using namespace testing;
+
+class CAccountingTest : public Test
+{
+protected:
+    virtual void SetUp()
+    {
+
+    }
+
+};
+
